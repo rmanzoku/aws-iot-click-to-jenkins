3e9908fa7d3b3dccf1ee58508571ddd3a31724a2
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-08-22 10:24:13 +0000

    OK

--- lambda_function.py
+++ lambda_function.py
@@ -10,6 +10,13 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+def single_click_function(attributes):
+    return {
+        "ENV": attributes.get("single_env"),
+        "VERSION": attributes.get("single_version"),
+    }
+
+
 def lambda_handler(event, context):
     jenkins_url = os.environ.get("JENKINS_URL")
     user = os.environ.get("USER")
@@ -20,23 +27,22 @@ def lambda_handler(event, context):
     job_name = event["placementInfo"]["attributes"].get("job")
     url = os.path.join(jenkins_url, "job", job_name, "buildWithParameters")
     headers = {}
-    logger.info("Request to " + url)
 
     if password is not None:
         basic_user_and_pasword = base64.b64encode((user+":"+password).encode("utf-8")).decode("utf-8")
         headers["Authorization"] = "Basic " + basic_user_and_pasword
 
-    params = {
-        "ENV": "HOGEGG",
-        "VERSION": "aaa",
-    }
+    clickType = event["deviceEvent"]["buttonClicked"]["clickType"]
+    if clickType == "SINGLE":
+        params = single_click_function(event["placementInfo"]["attributes"])
 
+    else:
+        logger.fatal("Not suppert deveceEvent: " + clickType)
+        exit(1)
+
+    logger.info("Request to " + url)
+    logger.info("Params is " + json.dumps(params))
     req = urllib.request.Request("{}?{}".format(url, urllib.parse.urlencode(params)),
                                  json.dumps(params).encode(),
                                  headers=headers)
-    # req = urllib.request.Request(url, json.dumps(params).encode(),
-    #                              headers=headers)
-    with urllib.request.urlopen(req) as res:
-        body = res.read()
-
-    logger.info(body)
+    urllib.request.urlopen(req)
